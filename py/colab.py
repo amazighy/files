b885de92f561abf4f5a58ddbe5a0f6660b5644bd
Yazid Mekhtoub <ymekhtoub@gmail.com>
2020-11-02 12:36:04 -0500

    finishes

--- py/colab.py
+++ py/colab.py
@@ -1,6 +1,12 @@
 # HOW TO MOUNT THE DRIVE AND UNZIP FILES
 
+
+from google.colab import files
 from zipfile import ZipFile
+from google.colab import drive
+drive.mount('/content/gdrive')
+
+uploaded = files.upload()
 file_name = file_path
 
 with ZipFile(file_name, 'r') as zip:
