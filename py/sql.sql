e1908b80836551aba695e7c2c8f81fda8485f3ae
amazighy <50556073+amazighy@users.noreply.github.com>
2020-11-12 02:37:00 -0500

    Delete sql.sql

--- py/sql.sql
+++
@@ -1,5 +0,0 @@
-!pip3 inatall sqlalchemy
-!pip3 inatall psycopg2 
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://myusername:mypassword@myhost:5432/mydatabase')
-df.to_sql('table_name', engine)
