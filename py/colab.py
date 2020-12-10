3bc4aab53927fe18c76fdae52318a4dbd8d02af1
amazighy <50556073+amazighy@users.noreply.github.com>
2020-12-10 18:31:15 -0500

    Update colab.py

--- py/colab.py
+++ py/colab.py
@@ -12,3 +12,5 @@ file_name = file_path
 with ZipFile(file_name, 'r') as zip:
     zip.extractall()
     print('Done')
+pip install openimages
+ oi_download_dataset --base_dir ~/MLFinal/OpenImgs --labels Zebra Binoculars Woman Man Tree --format pascal --limit 30
